b7811e2f1253f343c8526a70a263de2bcd4d9679
Pancho Labrador <plabradorjr@gmail.com>
2019-10-30 11:29:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,12 +1,17 @@
 class BankAccount
 
   attr_accessor :balance, :status
-  attr_reader :name 
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @balance = 1000
     @status = "open"
   end
+
+  def deposit(monies)
+    @balance += monies
+  end
+
 
 end
